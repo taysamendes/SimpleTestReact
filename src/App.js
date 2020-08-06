7613212908e8b8fdf71250d76815df7f768dec9e
TaysaMendes <taysa_smp@hotmail.com>
2020-08-06 00:15:25 -0300

    files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
+ 
+import Home from './screens/Home';
+import Detail from './screens/Detail';
+
+
 const App = () => {
-    return <h1>App</h1>
+    return (
+        <BrowserRouter>
+            <ul>
+                <li><Link to ="/"> Home </Link></li>
+                <li><Link to = "/detail"> Detail </Link></li>
+            </ul>
+            <Switch>
+                <Route path="/detail"> 
+                    <Detail />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    ); 
 };
 
 export default App;

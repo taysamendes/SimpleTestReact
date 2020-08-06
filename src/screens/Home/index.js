dbe4922225feeb3bb3ff106779dfdfca554b95d7
TaysaMendes <taysa_smp@hotmail.com>
2020-08-06 00:57:20 -0300

    characters

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import {doGet} from '../../helpers/APIHelper'
 
 const Home = () => {
 
-    doGet('/people').then(console.log);
-    
-    return <h1>Home</h1>
+    const [people, setPeople] = useState([]);
+
+    useEffect(()=>{
+        doGet('/people').then(response => setPeople(response.results)); 
+    }, []);
+
+    if(!people || !people.length){
+        return (<h1>Loading...</h1>)
+    }
+
+const peopleList = people.map(p => <li key={p.url}>{p.name}</li>)
+
+    return (<>
+        <h1>Characters</h1>
+        <ul>{peopleList}</ul>
+        </>
+    );
 };
 
 export default Home;
